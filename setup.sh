1660177321883ecb23444037fdd5dbfa07db6903
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-06-25 16:02:00 -0300

    Final version

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# rm -rf roles/galaxy
+rm -rf roles/galaxy
 ansible-galaxy install -p roles/galaxy/ -r roles/requirements.yml
 ansible-playbook -i assets/hosts site.yml -e username=$USER
 
