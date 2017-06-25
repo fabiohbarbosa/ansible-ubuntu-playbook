78599794f9f4dd5815e51698ada6de74f8fc2d57
Fabio Henrique Gonzales Barbosa <fabiohbarbosa@gmail.com>
2017-06-25 20:03:17 -0300

    Removing travis

--- setup.sh
+++ setup.sh
@@ -3,7 +3,4 @@ rm -rf roles/galaxy
 ansible-galaxy install -p roles/galaxy/ -r roles/requirements.yml
 ansible-playbook -i assets/hosts site.yml -e username=$USER
 
-# TODO migrar para role do docker
-sudo ln -sf /var/lib/docker /home/docker/
-
 echo 'Ansible install finished!'
