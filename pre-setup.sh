6969d728e864b99cd1f73e579eb878c80ff40a09
Fabio Henrique Gonzales Barbosa <fabiohbarbosa@gmail.com>
2017-06-26 21:29:10 -0300

    Increasing

--- pre-setup.sh
+++ pre-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo su -c "echo '$USER ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
-sudo apt-get install git ansible python-apt -y
+sudo apt-get install curl git ansible python-apt -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs -y
 git clone https://github.com/fabiohbarbosa/ansible-ubuntu-playbook.git
