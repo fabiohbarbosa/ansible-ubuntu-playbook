9703763dfeb526ffd21e932b2962e5fd175331c9
Fabio Henrique Gonzales Barbosa <fabiohbarbosa@gmail.com>
2017-06-28 23:34:50 -0300

    Cleaning repository

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# rm -rf roles/galaxy
-ansible-galaxy install -p roles/galaxy/ -r roles/requirements.yml
-ansible-playbook -i assets/hosts site.yml -e username=$USER
-
-echo 'Ansible install finished!'
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-image_name=fabiohbarbosa/ansible-runner:ubuntu-17.04-zesty
-USER=runner docker run -it --rm -v `pwd`:/runner --name ubuntu-playbook-test ${image_name} bash setup.sh
